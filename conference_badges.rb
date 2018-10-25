7e99d12cf786811c8993824f0b6c4ff3f44e2b0f
Cristina Kouri <cristina.isabel.kouri@gmail.com>
2018-10-25 21:36:49 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,6 +2,8 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end 
 
-def batch_badge_creator
-  
+def batch_badge_creator(name_array)
+  name_array.each do |name|
+    puts badge_maker(name)
+  end 
 end 
