11fcaa9db6c4744f41e88cd205ea8661d7125a3c
Cristina Kouri <cristina.isabel.kouri@gmail.com>
2018-10-25 21:32:24 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,19 +1,3 @@
-def badge_maker(person)
-  "Hello, my name is #{person}."
-end
-
-#return list of badges with 'hello my name is (name)'' from array of names as argument
-def batch_badge_creator(names)
-  list = []
-  names.each do |name|
-    list << badge_maker(name)
-  end
-  list
-end
-
-def assign_rooms(attendees)
-  blank = []
-  attendees.each_with_index {|attendee, index|
-    blank << "Hello #{speaker}! You'll be assigned to room #{index + 1}!" }
-    return blank
-end
+def badge_maker
+  
+end 
